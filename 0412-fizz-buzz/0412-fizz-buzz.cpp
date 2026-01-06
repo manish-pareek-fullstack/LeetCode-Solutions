9f5ca6e433e0b846dbb51a10fcb462b2190a8a02
manish pareek <manishpareek.3334@gmail.com>
2026-01-06 20:55:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (41.81%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> num;
+        int i = 1;
+        while (i <= n) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                num.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                num.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                num.push_back("Buzz");
+            }
+
+            else {
+                num.push_back(to_string(i));
+            }
+            i++;
+        }
+        return num;
+    }
+};
