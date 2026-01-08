cab8dc857479714725a43312f792766dc3f7940a
manish pareek <manishpareek.3334@gmail.com>
2026-01-08 19:32:16 +0530

    Time: 0 ms (100%), Space: 7.8 MB (85.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
 
-        int prev1 = 2; 
-        int prev2 = 1; 
+        int prev1 = 2;
+        int prev2 = 1;
         int curr;
 
         for (int i = 3; i <= n; i++) {
