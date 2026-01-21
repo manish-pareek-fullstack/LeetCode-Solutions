33b821e3585c3e977396e478f5e25086a2a2720c
Manish pareek <manishpareek.3334@gmail.com>
2026-01-21 11:51:03 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.12%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=INT_MIN;
+        int sum=0;
+        for(auto x:nums){
+         sum+=x;
+         ans=max(ans,sum);
+         if(sum<0){
+            sum=0;
+         }
+         
+        }
+        return ans;
+    }
+};
