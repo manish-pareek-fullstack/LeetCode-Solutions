74c970b0372ca8f591d8c08610426f43b218dc77
Manish pareek <manishpareek.3334@gmail.com>
2026-02-03 00:40:32 +0530

    Time: 4 ms (27.72%), Space: 9.5 MB (33.97%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for (auto x : s) {
+            if (x == 'i') {
+                reverse(ans.begin(), ans.end());
+            } else {
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
