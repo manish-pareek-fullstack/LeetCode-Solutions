1731ff353de4832723e37c235b6fcaf03667e131
Manish pareek <manishpareek.3334@gmail.com>
2026-02-06 10:53:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (18.69%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        map<char,int>m;
+        bool flag=false;
+        for(auto x:s){
+            m[x]++;
+        }
+        int store=m[s[0]];
+
+        for(auto x:m){
+            if(x.second!=store){
+                return false;
+            }
+        }
+        return true;
+       
+       
+    }
+};
