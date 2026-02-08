53f11a72f1f59036b5def24ac67e42a137d99a24
Manish pareek <manishpareek.3334@gmail.com>
2026-02-09 01:47:33 +0530

    Time: 3 ms (18.99%), Space: 10.2 MB (10.82%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char>st;
+        string ans="";
+        for(auto x:s){
+            if(x=='('){
+                if(!st.empty()){
+                    ans+='(';
+                }
+                st.push('(');
+            }
+            else{
+                st.pop();
+                if(!st.empty()){
+                    ans+=')';
+                }
+            }
+        }
+        return ans;
+    }
+};
