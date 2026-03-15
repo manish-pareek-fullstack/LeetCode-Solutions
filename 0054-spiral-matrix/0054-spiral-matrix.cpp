7a36997ba91942927570846af45a382ad5855796
Manish pareek <manishpareek.3334@gmail.com>
2026-03-16 03:15:20 +0530

    Time: 0 ms (100%), Space: 9.4 MB (51.32%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int top = 0, bottom = n - 1;
-        int left = 0, right = m - 1;
+        int left = 0, right = m - 1, top = 0, bottom = n - 1;
         vector<int> ans;
-        while (top <= bottom && left <= right) {
+        while (left <= right && top <= bottom) {
             for (int i = left; i <= right; i++) {
                 ans.push_back(matrix[top][i]);
             }
             top++;
+
             for (int i = top; i <= bottom; i++) {
                 ans.push_back(matrix[i][right]);
             }
