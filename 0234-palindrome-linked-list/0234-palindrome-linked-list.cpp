5461f6e9444611f5991b7f44155c342a72843e12
Manish pareek <manishpareek.3334@gmail.com>
2026-04-03 02:55:59 +0530

    Time: 9 ms (16.01%), Space: 134.7 MB (5.3%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,45 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode*slow=head;
-        ListNode*fast=head;
-        ListNode*temp=NULL;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }    
-        temp=slow->next;
-        slow->next=NULL;
-        ListNode*prev=NULL;
-        ListNode*curr=temp;
-        ListNode*front=head;
-        while(curr!=NULL){
-            front=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=front;
+        vector<int> v;
+        ListNode* temp = head;
+        while(temp != NULL){
+            v.push_back(temp->val);
+            temp = temp->next;
         }
-        ListNode*mid=head;
-        ListNode*second=prev;
-         while(second != NULL){
-            if(mid->val != second->val){
-                return false;
-            }
-            mid = mid->next;
-            second = second->next;
-        }
-        return true;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* front = head;
 
-        
+        while(curr != NULL){
+            front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
+        }
+        vector<int> ans;
+        temp = prev;
+        while(temp != NULL){
+            ans.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        return (v == ans);
     }
 };
